35885906e1dfcba91a8d7cb4dfba6d79ecca73b0
Roberto Salgado <rsalgado03@gmail.com>
2014-01-24 12:59:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Studium::Application.config.secret_key_base = '7fc3482e420fffc36512d69fa8ecbcfa8098f9b06f83c4a37825c04648aa215c637a8240b8da22f080d84f86c45806ea0591de3b873bd3693f1e9e496e2b3c57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Studium::Application.config.session_store :cookie_store, key: '_studium_session'
