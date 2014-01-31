62c34de965d087c9bed054dc87435ceed33cd9a4
Roberto Salgado <rsalgado03@gmail.com>
2014-01-30 21:43:49 -0500

    Implement index, new and create actions with corresponding views; create partial views for section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,5 +1,30 @@
 class SectionsController < ApplicationController
+
   def index
+    @reading = Reading.find params[:reading_id]
+    @sections = @reading.sections
+  end
+
+  def new
+    @reading = Reading.find params[:reading_id]
+    @sections = @reading.sections
+    @section = Section.new
+    @section.reading = @reading
+  end
+
+  def create
+    @reading = Reading.find params[:reading_id]
+    @sections = @reading.sections
+
+    strong_params = params.require(:section).permit(:name)
+    @section = Section.new strong_params
+    @section.reading = @reading
+
+    if @section.save
+      redirect_to reading_sections_path(@reading)
+    else
+      render 'new'
+    end
   end
   
 end
